42cc9b4d5124837bed0320373347b40762e1f320
Cláudio Lemos <cflemos1999@me.com>
2018-12-12 18:50:45 +0000

    Removed debugging

--- actions/api_sort.php
+++ actions/api_sort.php
@@ -3,10 +3,6 @@
   include_once(__DIR__."/../database/posts.php");
   include_once(__DIR__."/../database/users.php");
 
-  ob_start();
-  var_dump($_POST);
-  error_log(ob_get_clean(), 4);
-
   if($_POST['subscribed'] == "true")
     echo json_encode(getSubscribedPosts($_POST['sort'], getUserID($_SESSION['username'])));
   else
