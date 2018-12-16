983e873a52084accf35f42cc093ce5266832f999
Cláudio Lemos <cflemos1999@me.com>
2018-12-16 02:57:56 +0000

    Fixed directories error

--- templates/common/header.php
+++ templates/common/header.php
@@ -10,18 +10,18 @@
   <title>Reddit</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/style.css" rel="stylesheet">
-  <link href="css/layout.css" rel="stylesheet">
-  <link href="css/responsive.css" rel="stylesheet">
-  <link href="css/comments.css" rel="stylesheet">
-  <link href="css/forms.css" rel="stylesheet">
+  <link href="/css/style.css" rel="stylesheet">
+  <link href="/css/layout.css" rel="stylesheet">
+  <link href="/css/responsive.css" rel="stylesheet">
+  <link href="/css/comments.css" rel="stylesheet">
+  <link href="/css/forms.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
   <header>
     <div id="logo">
-      <a href="/"><img src="images/reddit.png" alt="Reddit logo" style="width:75px; height:75px;"></a>
+      <a href="/"><img src="/images/reddit.png" alt="Reddit logo" style="width:75px; height:75px;"></a>
       <h1><a href="/">reddit</a></h1>
     </div>
     <nav id="channels">
