ced51d3c83857dcd06811ef872d5ac5bdbd1c347
Cláudio Lemos <cflemos1999@me.com>
2018-12-16 21:33:35 +0000

    Fixed directories

--- templates/common/footer.php
+++ templates/common/footer.php
@@ -3,15 +3,15 @@
 </footer>
 </body>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="../../js/post.js"></script>
-<script src="../js/utils.js" defer></script>
-<script src="../js/postVoting.js" defer></script>
-<script src="../js/commentsVoting.js" defer></script>
-<script src="../js/sort.js" defer></script>
-<script src="../js/register.js" defer></script>
-<script src="../js/login.js" defer></script>
-<script src="js/settings.js" defer></script>
-<script src="../js/subscribe.js" defer></script>
-<script src="../js/createChannel.js" defer></script>
-<script src="../js/loadMore.js" defer></script>
+<script src="/js/post.js"></script>
+<script src="/js/utils.js" defer></script>
+<script src="/js/postVoting.js" defer></script>
+<script src="/js/commentsVoting.js" defer></script>
+<script src="/js/sort.js" defer></script>
+<script src="/js/register.js" defer></script>
+<script src="/js/login.js" defer></script>
+<script src="/js/settings.js" defer></script>
+<script src="/js/subscribe.js" defer></script>
+<script src="/js/createChannel.js" defer></script>
+<script src="/js/loadMore.js" defer></script>
 </html>
